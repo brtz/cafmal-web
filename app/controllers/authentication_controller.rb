73dde9c86002ef46d5c88b5cfeeed16a26c52a21
Stefan Piep <Stefan Piep>
2016-11-22 22:19:31 +0100

    used cafmal gem, login, resources list

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,23 +1,17 @@
 class AuthenticationController < ApplicationController
-  before_action :test
-
-  def test
-    p params[:action]
-    p @cafmal_auth
-  end
-
   def login
     @title = "Login"
   end
 
   def logout
     delete_cafmal_api_token
-    @cafmal_auth.logout
+    #@cafmal_auth.logout
     flash[:notice] = "Farewell!"
     redirect_to login_path
   end
 
   def auth
+    @cafmal_auth = Cafmal::Auth.new(Rails.application.secrets.cafmal_api_url)
     @cafmal_auth.login( user_params[:email], user_params[:password] )
 
     unless @cafmal_auth.token.nil?
